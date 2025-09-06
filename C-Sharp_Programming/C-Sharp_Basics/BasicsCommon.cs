981ccd214c6b3066bfe655cd35ea9234bc177ddf
Pradeep Jadav <jadavpradeep88p@gmail.com>
2025-09-06 16:01:53 +0530

    Add C_Sharp_Basics project and update solution files
    - Updated `.gitignore` to exclude user-specific and auto-generated files.
    - Introduced `C_Sharp_Basics` namespace with `BasicsCommon` class.
    - Created `C-Sharp_Basics.csproj` targeting .NET 9.0 with necessary settings.
    - Updated solution file to include the new `C-Sharp_Basics` project.
    - Created `C-Sharp_Programs.csproj` referencing `C-Sharp_Basics`.
    - Modified `Program.cs` to print greeting from `BasicsCommon`.

---
+++ C-Sharp_Programming/C-Sharp_Basics/BasicsCommon.cs
@@ -0,0 +1,11 @@
+﻿namespace C_Sharp_Basics
+{
+    public class BasicsCommon
+    {
+        const string greeting = "Hello, World!";
+        public static string GetGreeting()
+        {
+            return greeting;
+        }
+    }
+}
---
+++ C-Sharp_Programming/C-Sharp_Programs/Program.cs
@@ -0,0 +1,9 @@
+﻿using C_Sharp_Basics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(BasicsCommon.GetGreeting());
+    }
+}
